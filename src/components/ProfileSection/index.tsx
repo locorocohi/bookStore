5e01a153a2508dc6985a48d418d6857ce3b028c4
Artem Rudov <dovakin2281337@gmail.com>
2024-08-06 15:48:06 +0300

    add profile page styles

--- src/components/ProfileSection/index.tsx
+++ src/components/ProfileSection/index.tsx
@@ -1,11 +1,64 @@
 import Image from 'next/image';
+
+import passwordCloseEye from '@/images/hide.svg';
+import emailIcon from '@/images/email.svg';
+import openEye from '@/images/openEye.svg';
+import profilePic from '@/images/profile.svg';
+import chelik from '@/images/chelik.svg';
+import cameraPic from '@/images/button_photo.svg';
+
 import { Wrapper } from './styles';
+import Input from '../Input';
+import PasswordInput from '../PasswordInput';
 
 const ProfileSection = () => {
   return (
     <Wrapper>
+      <div className=".avatar">
+        <Image src={chelik} alt="avatar" />
+        <button type="button" className="button">
+          <Image src={cameraPic} alt="photo" />
+        </button>
+      </div>
 
-      <Image />
+      <div className="profile-info">
+        <div className="profile-personal">
+          <div className="profile-title">
+            <h3>Personal information</h3>
+            <button>Change information</button>
+          </div>
+          <Input
+            value="Guy Hawkins"
+            signature="Your name"
+          >
+            <Image src={profilePic} alt="profile"
+              width={24} height={24}
+            />
+          </Input>
+
+          <Input
+          value="test@example.ru"
+          signature="Your email"
+          >
+            <Image src={emailIcon} alt="email"
+              width={24} height={24}
+            />
+          </Input>
+        </div>
+        <div className="profile-password">
+          <div className="profile-title">
+            <h3>Password</h3>
+            <button>Change password</button>
+          </div>
+          <PasswordInput
+          value="testasdasd"
+          signature="Your password"
+          >
+           <Image src={passwordCloseEye} alt="eye" priority />
+           <Image src={openEye} alt="view" priority />
+          </PasswordInput>
+        </div>
+      </div>
 
     </Wrapper>
   );
--- src/components/ProfileSection/styles.ts
+++ src/components/ProfileSection/styles.ts
@@ -1,5 +1,64 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.section`
-  display: flex;
+  display: grid;
+  padding: 60px 80px;
+  grid-template-columns: 1.5fr 2fr 1fr;
+
+  .avatar {
+    position: relative;
+    width: 305px;
+    height: 305px;
+    border-radius: 16px;
+  }
+
+  .button {
+    position: absolute;
+    width: 48px;
+    height: 48px;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+
+    top: 405px;
+    left: 325px;
+    }
+
+  .profile-info {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 522px;
+    gap: 40px;
+  }
+
+  .profile-title{
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 10px;
+  }
+
+  .profile-title h3{
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 30px;
+  }
+
+  .profile-title button{
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 21px;
+    text-decoration: underline;
+    text-align: center;
+    color: rgba(141, 159, 79, 1);
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
+
+  .profile-personal, .profile-password{
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
 `;
