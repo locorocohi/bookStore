f236523f1f3f663d7e36e16b61d60e6c96578db0
Artem Rudov <dovakin2281337@gmail.com>
2024-08-26 17:34:46 +0300

    add Cart page

---
+++ src/components/EmptyCartSection/EmptyCartSection.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import books from '@/images/cartBooks.svg';
+
+import { Wrapper } from './styles';
+
+const EmptyCartSection = () => {
+  return (
+    <Wrapper>
+      <Image src={books} alt="books" />
+      <div>
+        <h1>Your cart is empty</h1>
+        <p>Add items to cart to make a purchase. Go to the catalog now.</p>
+        <Link className="redirect-button" href="/">Go to catalog</Link>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default EmptyCartSection;
