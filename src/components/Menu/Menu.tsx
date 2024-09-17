2e071727a22ea8fccd16afd88e003510429c8a2a
Artem Rudov <dovakin2281337@gmail.com>
2024-09-17 14:15:35 +0300

    update headers Menu with Link

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,42 +1,26 @@
-import { useRouter } from 'next/router';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import cart from '@/images/Cart.svg';
 import profile from '@/images/profile-button.svg';
 import favorite from '@/images/Heart.svg';
-import { config } from '../../../config';
 
-import Button from '../Button';
 import { Wrapper } from './styles';
 
 const Menu = () => {
-  const router = useRouter();
-
-  const redirectToFavorite = () => {
-    router.push(new URL(`http://${config.HOST}:${config.LOCAL_PORT}/favorite`));
-  };
-
-  const redirectToCart = () => {
-    router.push(new URL(`http://${config.HOST}:${config.LOCAL_PORT}/cart`));
-  };
-
-  const redirectToProfile = () => {
-    router.push(new URL(`http://${config.HOST}:${config.LOCAL_PORT}/profile`));
-  };
-
   return (
     <Wrapper>
-      <Button type="button" className="button" onClick={redirectToCart}>
+      <Link href="/cart" className="button">
         <Image src={cart} alt="Cart" />
-      </Button>
+      </Link>
 
-      <Button type="button" className="button" onClick={redirectToFavorite}>
+      <Link href="/favorite" className="button">
         <Image src={favorite} alt="Favorite" />
-      </Button>
+      </Link>
 
-      <Button type="button" className="button" onClick={redirectToProfile}>
+      <Link href="/profile" className="button">
         <Image src={profile} alt="Profile" />
-      </Button>
+      </Link>
     </Wrapper>
   );
 };
